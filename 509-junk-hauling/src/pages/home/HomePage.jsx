7269fa5e85440583962fd34ee04aa635456d8aad
Benjamin Davidsone <bendavidson5309@icloud.comm>
2025-04-19 10:20:03 -0700

    Moved.gitignore and README.md to 509-junk-hauling

---
+++ 509-junk-hauling/src/pages/home/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to Our Junk Hauling Service</h1>
+      </div>
+  );
+};
--- server/index.js
+++ server/index.js
@@ -4,17 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the public folder
-app.use(express.static(path.join(__dirname, '../public')));
+// Serve static files from the React app's build folder
+app.use(express.static(path.join(__dirname, '../509-junk-hauling/build')));
 
-// Example API route
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from the server!' });
-});
-
-// Catch-all route to serve the index.html file
+// Catch-all route to serve the React app's index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../509-junk-hauling/build/index.html'));
 });
 
 // Start the server
